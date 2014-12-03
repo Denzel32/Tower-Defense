f528f1887f34f75b63b585625ab358d9f79058ef
Denzel <denzel_justin@hotmail.com>
2014-12-03 12:08:52 +0100

    First commit, pathfinding works

---
+++ Assets/Scripts/EnemyDestination.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDestination : MonoBehaviour 
+{	
+	private NavMeshAgent agent;
+	[SerializeField]
+	private GameObject target;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoints : MonoBehaviour 
+{	public Transform [] spawnPoints;	// the spawnpoints for where the enemies will be spawning
+	public GameObject enemy;			//the gameobject that contains the enemy.
+	private int maxEnemies = 0;
+	// Use this for initialization
+	void Start () 
+	{	
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(maxEnemies < 3)
+		{
+			Spawn();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			maxEnemies--;
+		}
+	}
+
+	void Spawn()
+	{
+		for(int i = 0; i < maxEnemies; i++)
+		{
+			//instantiates the enemy gameobject and puts it into the position of the spawnpoints.
+			//random.range picks one of the spawnpoints at random and that is where the enemy is spawned.
+			Instantiate(enemy,spawnPoints[Random.Range(0,5)].position, transform.rotation);
+
+		}
+		maxEnemies++;
+	}
+
+
+}
